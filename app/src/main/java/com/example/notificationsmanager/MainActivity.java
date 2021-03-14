4909660802c846bd4ce2a25698286fa931691a35
MikeC0xl0ng <bodliev.mykola@gmail.com>
2021-03-14 09:31:53 +0100

    Moving the notification code in the "notificationAppear()" method

--- app/src/main/java/com/example/notificationsmanager/MainActivity.java
+++ app/src/main/java/com/example/notificationsmanager/MainActivity.java
@@ -22,50 +22,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn = (Button) findViewById(R.id.button);
-        String channel_description = "channel_description";
-        String channel_ID = "my_channel_ID";
-        String channel_name = "channel_name";
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            NotificationChannel channel = new NotificationChannel(channel_ID,
-                    channel_name,
-                    NotificationManager.IMPORTANCE_DEFAULT);
-            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-            channel.setDescription(channel_description);
-            notificationManager.createNotificationChannel(channel);
-        }
-
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-                NotificationCompat.Builder builder  = new NotificationCompat.Builder(MainActivity.this, channel_ID)
-                        .setContentTitle("Title of the notification")
-                        .setContentText("Subject of the notification")
-                        .setSmallIcon(R.drawable.ic_launcher_foreground)
-                        .setAutoCancel(true);
-
-                NotificationManager managerCompat = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-                managerCompat.notify(0, builder.build());
-
-
+                notificationAppear();
             }
         });
     }
 
-    private void notificationAppear(double seconds) {
-
-    }
-
     private void notificationAppear() {
 
-        /*
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         String channel_ID = "my_channel_ID";
         String channel_name = "channel_name";
         String channel_description = "channel_description";
 
-        if(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             NotificationChannel channel = new NotificationChannel(channel_ID,
                     channel_name,
                     NotificationManager.IMPORTANCE_DEFAULT);
@@ -73,22 +45,12 @@ public class MainActivity extends AppCompatActivity {
             notificationManager.createNotificationChannel(channel);
         }
 
-        NotificationCompat.Builder builder  = new NotificationCompat.Builder(this)
-                .setContentTitle("New mail from " + "test@gmail.com")
-                .setContentText("Subject")
+        NotificationCompat.Builder builder  = new NotificationCompat.Builder(this, channel_ID)
+                .setContentTitle("Notification's title")
+                .setContentText("Subject of the notification")
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentIntent(pIntent)
-                .setAutoCancel(true)
-                .addAction(R.drawable.icon, "Call", pIntent)
-                .addAction(R.drawable.icon, "More", pIntent)
-                .addAction(R.drawable.icon, "And more", pIntent).build();
+                .setAutoCancel(true);
 
         notificationManager.notify(0, builder.build());
-
-
-         */
-
-
     }
-
 }
